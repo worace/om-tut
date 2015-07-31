cd0da7581973ca28e4359c06d1331a7330ce8d82
worace <horace.d.williams@gmail.com>
2015-07-30 20:14:32 -0600

    list of contacts

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -8,21 +8,47 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+(defonce app-state
+  (atom
+   {:contacts
+    [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+     {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+     {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+     {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+     {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+     {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+
+(defn by-id [id] (.getElementById js/document id))
+
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
+
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
+
+(defn contacts-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+               (dom/h2 nil "Contact List:")
+               (apply dom/ul nil
+                      (om/build-all contact-view (:contacts data)))))))
 
 (defn stripe [text bgc]
   (let [style #js {:backgroundColor bgc}]
     (dom/li #js {:style style} text)))
 
-(om/root
-  (fn [data owner]
-    (om/component
-     (apply dom/ul #js {:className "animals"}
-            (map stripe
-                 (data :list)
-                 (cycle ["#ff0" "#fff"])))))
-  app-state
-  {:target (. js/document (getElementById "app0"))})
+(om/root contacts-view
+         app-state
+         {:target (by-id "contacts")})
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
