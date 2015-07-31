cc0ec97a9a315aa65229b5d8d98ad0561ca499f2
worace <horace.d.williams@gmail.com>
2015-07-30 20:01:26 -0600

    multiroot

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -12,11 +12,16 @@
 
 (om/root
   (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
+    (om/component (dom/h2 nil (:text data))))
   app-state
-  {:target (. js/document (getElementById "app"))})
+  {:target (. js/document (getElementById "app0"))})
+
+(om/root
+ (fn [data owner]
+   (om/component (dom/h2 nil (:text data))))
+ app-state
+ {:target (. js/document (getElementById "app1"))})
+
 
 
 (defn on-js-reload []
